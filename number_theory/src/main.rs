9718430f59e6c95e569d653000852de46dd1c936
Ivan <yifanb@gmail.com>
2017-02-22 00:47:49 +0000

    更新内容

--- number_theory/src/main.rs
+++ number_theory/src/main.rs
@@ -1,20 +1,20 @@
-
-
 ///手动实现 最小公倍数、最大公约数 等功能
 ///其实Rust已经有相关实现的std::num
 /// 作者:yfgeek
 
 // 两数交换
 // mem::swap(&mut x, &mut y);
-fn swap(x: i32, y: i32)->(i32,i32){
-    (y,x)
+fn swap(x: &mut i32, y: &mut i32){
+   let tmp = *x;
+    *x = *y;
+    *y = tmp;
 }
 
 // 最小公倍数
 // lcm<T>(x: T, y: T) -> T
-fn lcm(x : i32, y: i32) ->i32{
+fn lcm(mut x : i32, mut y: i32) ->i32{
     if x<y{
-        swap(x,y);
+        swap(&mut x,&mut y);
     }
     let mut max = x;
     while max % y != 0{
@@ -31,7 +31,10 @@ fn gcd(x : i32, y: i32) ->i32{
 }
 
 fn main() {
-    println!("24和36交换结果是 :{:?}",swap(24,36));
-    println!("24和36的最大公约数是 :{}",gcd(24,36));
-    println!("24和36的最小公倍数是 :{}",lcm(24,36));
+    let mut m = 24;
+    let mut n = 36;
+    swap(&mut m,&mut n);
+    println!("{}和{}交换结果是 :{:?}",m,n,(m,n));
+    println!("{}和{}的最大公约数是 :{}",m,n,gcd(m,n));
+    println!("{}和{}的最小公倍数是 :{}",m,n,lcm(m,n));
 }
